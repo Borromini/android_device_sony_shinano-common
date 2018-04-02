2fe44e4965982f7234b4994d61bd810cd4e6ae17
Stijn Segers <foss@volatilesystems.org>
2018-04-02 21:59:20 +0200

    shinano: init: fix compilation dependencies for Oreo
    * Remove the vendor_init header as system/core is using a different mechanism now.
    * Include the required libsepolicy
    
    Similar to OmniROM 9cb9e6b5c0871e4131f3d6fdd11f4eb5b96b95f1 and CarbonROM 69fdafeea59ae40d6fbd163f5b9437ecc30ab791 - we had already implemented the android::init namespace, including libselinux allows us to strip external/selinux/libselinux/include from LOCAL_C_INCLUDES as well.
    
    Signed-off-by: Stijn Segers <foss@volatilesystems.org>

--- init/Android.mk
+++ init/Android.mk
@@ -5,11 +5,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
-	system/core/base/include \
 	system/core/init \
-	external/selinux/libselinux/include
 LOCAL_CFLAGS := -Wall
-LOCAL_STATIC_LIBRARIES := libbase
+LOCAL_STATIC_LIBRARIES := libbase libselinux
 LOCAL_SRC_FILES := init_shinano.cpp
 LOCAL_MODULE := libinit_shinano
 include $(BUILD_STATIC_LIBRARY)
